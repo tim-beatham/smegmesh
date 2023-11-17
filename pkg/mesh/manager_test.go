36c264b38e2942c6ce837c8d1de1eca9d2196081
Tim Beatham <tcb1@st-andrews.ac.uk>
2023-11-17 22:18:53 +0000

    25-ability-aliases
    Fixed unit tests failing

--- pkg/mesh/manager_test.go
+++ pkg/mesh/manager_test.go
@@ -51,7 +51,7 @@ func TestCreateMeshCreatesANewMeshProvider(t *testing.T) {
 		t.Fatal(`meshId should not be empty`)
 	}
 
-	_, exists := manager.Meshes[meshId]
+	_, exists := manager.GetMeshes()[meshId]
 
 	if !exists {
 		t.Fatal(`mesh was not created when it should be`)
@@ -190,7 +190,7 @@ func TestLeaveMeshDeletesMesh(t *testing.T) {
 		t.Fatalf("%s", err.Error())
 	}
 
-	_, exists := manager.Meshes[meshId]
+	_, exists := manager.GetMeshes()[meshId]
 
 	if exists {
 		t.Fatalf(`expected mesh to have been deleted`)
